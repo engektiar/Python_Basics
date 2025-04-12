89725b95559c4d31ba6aa7377adaaf55e55a5734
engektiar <eng.ektiar@gmail.com>
2025-04-12 21:30:53 +0600

    data type add

--- datatype.py
+++ datatype.py
@@ -7,3 +7,31 @@ print(name," is a string")
 
 x=35
 print("thsi is intger",x)
+
+y=3.36
+print(y ,"is a float")
+
+z=1j
+print(z,"is a complex number")
+
+x=['ektuiar','hossen','khan']
+print(x,"is a list")
+
+x=("apple","banana","cherry")
+print(x,"is a tuple")
+
+x=range(6)
+print(x,"is a set")
+
+x = {"name" : "John", "age" : 36 ,"city" : "New York","ektriar":42}
+print(x,"is a dictionary")
+
+
+x = {"apple", "banana", "cherry"}
+print(x,"\n ekdareir")
+
+x = frozenset({"apple", "banana", "cherry"})
+print(x,"is a frozen set")
+
+x = memoryview(bytes(5))
+print(x,"is a memory view")
